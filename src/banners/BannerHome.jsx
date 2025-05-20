98dfb247d9691c0d3925a03745d0cd206c529ddf
reinzenn <reinzen@gmail.com>
2025-05-20 14:34:00 -0300

    update banner home

--- src/banners/BannerHome.jsx
+++ src/banners/BannerHome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Banners.css"; // Import your CSS file for custom styles
+import "./banners.css"; // Import your CSS file for custom styles
 
 const BannerHome = () => {
   return (
